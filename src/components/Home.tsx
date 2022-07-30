69bb7a97c50085dffc0d041b3f80682ce1233853
mahdi parivash <mahdiparivash@protonmail.com>
2022-07-30 15:24:29 +0430

    top 10 crypto home complate

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,12 +3,16 @@ import millify from "millify";
 import { Row, Col, Statistic, Typography } from "antd";
 import { useGetCryptosQuery } from "../services/cryptoApi";
 import Loading from "./Loaidng";
+import { Link } from "react-router-dom";
+import Cryptocurrencies from "./Cryptocurrencies";
+import News from "./News";
 type HomeProps = {};
 
 const { Title } = Typography;
 
 const Home: React.FC<HomeProps> = () => {
   const { data, isFetching, error } = useGetCryptosQuery(10);
+  const globalData = data?.data?.stats;
 
   if (isFetching) return <Loading />;
 
@@ -19,41 +23,51 @@ const Home: React.FC<HomeProps> = () => {
       </Title>
       <Row gutter={[16, 16]}>
         <Col span={12}>
-          <Statistic
-            title="Total Crypto Currencies"
-            value={millify(1234, { precision: 1 })}
-            suffix="+"
-          />
+          <Statistic title="Total Crypto Currencies" value={globalData.total} />
         </Col>
         <Col span={12}>
           <Statistic
             title="Total Crypto Exchanges"
-            value={millify(1234, { precision: 1 })}
-            suffix="+"
+            value={millify(globalData.totalExchanges)}
           />
         </Col>
         <Col span={12}>
           <Statistic
             title="Total Market Cap"
-            value={millify(1234, { precision: 1 })}
-            suffix="+"
+            value={millify(globalData.totalMarketCap)}
           />
         </Col>
         <Col span={12}>
           <Statistic
             title="Total 24h Volume"
-            value={millify(1234, { precision: 1 })}
-            suffix="+"
+            value={millify(globalData.total24hVolume)}
           />
         </Col>
         <Col span={12}>
           <Statistic
             title="Total Markets"
-            value={millify(1234, { precision: 1 })}
-            suffix="+"
+            value={millify(globalData.totalMarkets)}
           />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News />
     </>
   );
 };
